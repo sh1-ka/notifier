7daa95712fd511a3eaa42c9a6584758c15fb6881
sh1ka <u1nder3taker@gmail.com>
2026-06-30 15:18:56 +0300

    l

---
+++ db.py
@@ -0,0 +1,94 @@
+import asyncio
+from typing import List
+from sqlalchemy import ForeignKey, select, String, Boolean, exists, delete
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine, create_async_engine, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, selectinload, relationship
+import uuid
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "user"
+
+    id : Mapped[int] = mapped_column(primary_key=True)
+    userid : Mapped[int] = mapped_column()
+    msg_id : Mapped[int | None] = mapped_column(nullable=True)
+
+    tasks : Mapped[List["Task"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+
+class Task(Base):
+    __tablename__ = "task"
+    id : Mapped[int] = mapped_column(primary_key=True)
+    title : Mapped[String] = mapped_column(String())
+    zid : Mapped[String] = mapped_column(String())
+    userid : Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user : Mapped[User] = relationship(back_populates="tasks")
+
+
+class DB():
+    def __init__(self, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB):
+        self.DATABASE = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}"
+        self.engine = create_async_engine(self.DATABASE)
+        self.session = async_sessionmaker(self.engine, expire_on_commit=False)
+
+    async def check_user(self, id : int):
+        async with self.session() as session:
+            stmt = select(User).where(User.userid == id)
+            usr = await session.scalar(stmt)
+            if not usr:
+                return False
+            return True
+
+    async def add_user(self, id : int):
+        async with self.session() as session:
+            cur_user = User()
+            cur_user.userid = id
+            session.add(cur_user)
+            await session.commit()
+
+    async def add_task_for_user(self, userid : int, titl : str, idd : str):
+        async with self.session() as session:
+            stmt = select(User).where(User.userid == userid).options(selectinload(User.tasks))
+            usr = await session.scalar(stmt)
+            usr.tasks.append(Task(title=titl, zid=idd))
+            await session.commit()
+            print("Таска добавлена")
+    
+    async def delete_task(self, id : str):
+        async with self.session() as session:
+            stmt = delete(Task).where(Task.zid == id)
+            await session.execute(stmt)
+            await session.commit()
+            print("Таска удалена")
+    
+    
+    async def update_msg_id(self, id : int, nw_msg_id : int):
+        async with self.session() as session:
+            stmt = select(User).where(User.userid == id)
+            usr = await session.scalar(stmt)
+            usr.msg_id = nw_msg_id
+            await session.commit()
+
+    async def get_msg_id(self, id : int):
+        async with self.session() as session:
+            stmt = select(User).where(User.userid == id)
+            usr = await session.scalar(stmt)
+            return usr.msg_id
+
+    async def get_list_of_tasks(self, userid : int):
+        async with self.session() as session:
+            stmt = select(User).where(User.userid == userid).options(selectinload(User.tasks))
+            my_usr = await session.scalar(stmt)
+            result = []
+            for tsk in my_usr.tasks:
+                result.append([tsk.title, tsk.zid])
+            return result
+
+    async def init_db(self):
+        async with self.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+    async def del_db(self):
+        async with self.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, CallbackQueryHandler, ConversationHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import uuid
+import db
+import asyncio
 
 from dotenv import load_dotenv
 from os import environ as env
@@ -15,47 +17,62 @@ logging.basicConfig(
 )
 
 ADDTASK = 1
+cdb = db.DB(env["POSTGRES_USER"], env["POSTGRES_PASSWORD"], env["POSTGRES_DB"])
+
+
+async def get_keyboard(userid : int):
+    zzs = await cdb.get_list_of_tasks(userid)
+    keys = []
+    for titl, cdat in zzs:
+        keys.append([InlineKeyboardButton(text=titl, callback_data=cdat)])
+    return keys
 
 async def start(update : Update, context: ContextTypes.DEFAULT_TYPE):
-    keyb = [
-        [InlineKeyboardButton("Добавить задачу", callback_data="add_task")]
-    ]
+    if not await cdb.check_user(update.effective_user.id):
+        await cdb.add_user(update.effective_user.id)
+        await cdb.add_task_for_user(update.effective_user.id, "Добавить задачу", "add_task")
 
-    if not context.user_data.get("buttons"):
-        context.user_data["buttons"] = keyb
-
-    mark = InlineKeyboardMarkup(context.user_data["buttons"])
+    mark = InlineKeyboardMarkup(await get_keyboard(update.effective_user.id))
     msg = await update.message.reply_text("Дароу, я кароч сохраняю твои дела и все в целом", reply_markup=mark)
-    context.user_data["msg_id"] = msg.id
+    # context.user_data["msg_id"] = msg.id
+    await cdb.update_msg_id(update.effective_user.id, msg.id)
 
 async def add_task(update : Update, context : ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
-    await context.bot.edit_message_reply_markup(chat_id=update.effective_chat.id, message_id=context.user_data["msg_id"])
-    await context.bot.edit_message_text("Напиши новую задачу", chat_id=update.effective_chat.id, message_id=context.user_data["msg_id"])
+    await context.bot.edit_message_reply_markup(chat_id=update.effective_chat.id, message_id=await cdb.get_msg_id(update.effective_user.id))
+    await context.bot.edit_message_text("Напиши новую задачу", chat_id=update.effective_chat.id, message_id=await cdb.get_msg_id(update.effective_user.id))
     return ADDTASK
 
 async def get_nae(update : Update, context : ContextTypes.DEFAULT_TYPE):
-    context.user_data["buttons"].append([InlineKeyboardButton(update.message.text, callback_data=str(uuid.uuid4()))])
+    # context.user_data["buttons"].append([InlineKeyboardButton(update.message.text, callback_data=str(uuid.uuid4()))])
+    await cdb.add_task_for_user(update.effective_user.id, update.message.text, str(uuid.uuid4()))
 
-    msg = await update.message.reply_text("Ваши задачи:", reply_markup=InlineKeyboardMarkup(context.user_data["buttons"]))
-    context.user_data["msg_id"] = msg.id
+    msg = await update.message.reply_text("Ваши задачи:", reply_markup=InlineKeyboardMarkup(await get_keyboard(update.effective_user.id)))
+    # context.user_data["msg_id"] = msg.id
+    await cdb.update_msg_id(update.effective_user.id, msg.id)
     return ConversationHandler.END
 
 async def remove_task(update : Update, context : ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
 
-    needed_id = -1
-    for i in range(len(context.user_data["buttons"])):
-        if context.user_data["buttons"][i][0].callback_data == query.data:
-            needed_id = i
+    # needed_id = -1
+    # for i in range(len(context.user_data["buttons"])):
+    #     if context.user_data["buttons"][i][0].callback_data == query.data:
+    #         needed_id = i
     
-    context.user_data["buttons"].pop(needed_id)
-    await context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=context.user_data["msg_id"], text="Ваши задачи:", reply_markup=InlineKeyboardMarkup(context.user_data["buttons"]))
+    # context.user_data["buttons"].pop(needed_id)
+
+    await cdb.delete_task(query.data)
+    await context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=await cdb.get_msg_id(update.effective_user.id), text="Ваши задачи:", reply_markup=InlineKeyboardMarkup(await get_keyboard(update.effective_user.id)))
+
+async def on_startup(application):
+    await cdb.init_db()
+
 
 if __name__ == "__main__":
-    app = ApplicationBuilder().token(env["TOKEN"]).build()
+    app = ApplicationBuilder().token(env["TOKEN"]).post_init(on_startup).build()
 
     app.add_handler(CommandHandler('start', start))
     app.add_handler(ConversationHandler(
@@ -67,5 +84,4 @@ if __name__ == "__main__":
     ))
 
     app.add_handler(CallbackQueryHandler(remove_task))
-
     app.run_polling()
